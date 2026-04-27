6950add17307f01354b42e1c53eac4f6d37bef29
Bhavik Pahuja <bhavikpahuja0@gmail.com>
2026-04-27 14:15:39 +0530

    Added parctical est files

--- Practical_EST/nQueens.java
+++ Practical_EST/nQueens.java
@@ -1,48 +1,53 @@
+import java.util.*;
+
 class Solution {
-  boolean check(int row, int col, int n, char b[][]) {
-    for (int i=0; i<row; i++) {
-      if (b[i][col] == 'Q') {
-        return false;
-      }
+    boolean check(int row, int col, int n, char b[][]) {
+        for (int i = 0; i < row; i++) {
+            if (b[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (b[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (b[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
     }
-    for (int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
-      if (b[i][j] == 'Q') {
-        return false;
-      }
+
+    List<String> construct(char b[][]) {
+        List<String> ans = new ArrayList<>();
+        for (char i[] : b) {
+            ans.add(new String(i));
+        }
+        return ans;
     }
-    for (int i=row-1, j=col+1; i>=0 && j<n; i--, j++) {
-      if (b[i][j] == 'Q') {
-        return false;
-      }
+
+    void rec(int row, int n, char b[][], List<List<String>> ans) {
+        if (row == n) {
+            ans.add(construct(b));
+        }
+        for (int i = 0; i < n; i++) {
+            if (check(row, i, n, b)) {
+                b[row][i] = 'Q';
+                rec(row + 1, n, b, ans);
+                b[row][i] = '.';
+            }
+        }
     }
-    return true;
-  }
-  List<String> construct(char b[][]) {
-    List<String> ans = new ArrayList<>();
-    for (char i[] : b) {
-      ans.add(new String(i));
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        char b[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(b[i], '.');
+        }
+        rec(0, n, b, ans);
+        return ans;
     }
-    return ans;
-  }
-  void rec(int row, int n, char b[][], List<List<String>> ans) {
-    if (row == n) {
-      ans.add(construct(b));
-    }
-    for (int i=0; i<n; i++) {
-      if (check(row, i, n, b)) {
-        b[row][i] = 'Q';
-        rec(row + 1, n, b, ans);
-        b[row][i] = '.';
-      }
-    }
-  }
-  public List<List<String>> solveNQueens(int n) {
-    List<List<String>> ans = new ArrayList<>();
-    char b[][] = new char[n][n];
-    for (int i=0; i<n; i++) {
-      Arrays.fill(b[i], '.');
-    }
-    rec(0, n, b, ans);
-    return ans;
-  }
 }
